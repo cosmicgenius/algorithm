10169197238038b80c8adb28d2ad34be1c45f628
cosmicgenius <anthonyywang05@gmail.com>
2021-09-05 19:05:34 -0700

    Move python files to python directory

--- python/test.py
+++ python/test.py
@@ -1,4 +1,5 @@
 from timeit import timeit
+from memory_profiler import profile
 import math
 import random
 
@@ -9,8 +10,9 @@ def run():
     def slow(n):
         a = math.floor(math.log(n, 2))
 
-    print(timeit(lambda: fast(random.randint(1, 10**50)), number=10000000))
-    print(timeit(lambda: slow(random.randint(1, 10**50)), number=10000000))
+    print(timeit(lambda: fast(random.randint(1, 10**50)), number=1000000))
+    print(timeit(lambda: slow(random.randint(1, 10**50)), number=1000000))
 
 if __name__ == '__main__':
+    run()
     run()
